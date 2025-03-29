9d0ecfdd4befe6f8f6e9e975d39aeec0aaf07458
narminmusayeva <narminmsyva@gmail.com>
2025-03-29 23:26:02 +0400

    done

---
+++ ES6_basic/2-arrow.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+export default function getNeighborhoodsList() {
+  this.sanFranciscoNeighborhoods = ["SOMA", "Union Square"];
+
+  this.addNeighborhood = (newNeighborhood) => {
+    this.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return this.sanFranciscoNeighborhoods;
+  };
+}
