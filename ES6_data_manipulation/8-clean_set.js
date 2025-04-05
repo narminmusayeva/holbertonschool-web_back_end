162e3cc34c418deb4341bb450f902887f800ef3d
narminmusayeva <narminmsyva@gmail.com>
2025-04-05 23:13:17 +0400

    done

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,10 +1,10 @@
- export default function cleanSet(set, startString) {
-  if (!startString || typeof startString !== "string") {
-    return "";
+export default function cleanSet(set, startString) {
+  if (!startString || typeof startString !== 'string') {
+    return '';
   }
 
   return [...set]
     .filter((value) => value.startsWith(startString))
     .map((value) => value.slice(startString.length))
-    .join("-");
+    .join('-');
 }
